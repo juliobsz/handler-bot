c5667052fc499b5ef91c5e2027ab42d9d2792dfb
* <reginarbs45@gmail.com>
2020-08-21 20:09:26 -0400

    ping corrigido²

--- commands/ping.js
+++ commands/ping.js
@@ -1,8 +1,8 @@
 const Discord = require('discord.js');
 
 module.exports = {
-    name: "help",
-    description: "list all commands!",
+    name: "ping",
+    description: "bot ping",
     async execute(client, message, args) {
         let embed = new Discord.MessageEmbed()
         .setAuthor(`Pong!`, client.user.displayAvatarURL())
